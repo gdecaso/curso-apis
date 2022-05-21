124a25f1759213c2fd20eeb871f0755a685c8a41
Guido de Caso <guido@dialpad.com>
2022-05-21 17:55:53 -0300

    monitoring

---
+++ heladeria-server-springboot/src/main/java/com/heladeriaapilia/configuration/MonitoringConfig.java
@@ -0,0 +1,17 @@
+package com.heladeriaapilia.configuration;
+
+import com.moesif.servlet.MoesifFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.servlet.Filter;
+
+@Configuration
+public class MonitoringConfig extends WebMvcConfigurerAdapter {
+
+  @Bean
+  public Filter moesifFilter() {
+    return new MoesifFilter("eyJhcHAiOiIzMTU6MjMwIiwidmVyIjoiMi4wIiwib3JnIjoiNTg2OjE5NCIsImlhdCI6MTY1MTM2MzIwMH0.WzvR_jZ1wkXsGnjpWaE2breDd7L3gflC7zBFCdlqOuI");
+  }
+}
