ddb11ac39e37962200e1d1927ce1a0b43fb76e6e
Guido de Caso <guido@dialpad.com>
2022-05-22 18:49:13 -0300

    mas tests restassured

--- heladeria-server-springboot/src/test/java/com/heladeriaapilia/integration/RestAssuredTest.java
+++ heladeria-server-springboot/src/test/java/com/heladeriaapilia/integration/RestAssuredTest.java
@@ -2,57 +2,68 @@ package com.heladeriaapilia.integration;
 
 import io.restassured.RestAssured;
 import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
+import java.util.Map;
+
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class RestAssuredTest {
 
   public static final String BASE_URI = "/guidodecaso/heladeria/1.0.0";
+  @BeforeAll
+  static void beforeAll() {
+    RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
+  }
+
   @LocalServerPort
   private Integer port;
+
+  private String apiPath() {
+    return "http://localhost:" + port + BASE_URI;
+  }
 
   @Test
   void tieneQueHaberDulceDeLeche() {
     RestAssured
-            .given()
-              .filter(new RequestLoggingFilter())
-              .contentType(ContentType.JSON)
             .when()
-              .get("http://localhost:" + port + BASE_URI + "/gustos")
+              .get(apiPath() + "/gustos")
             .then()
               .statusCode(200)
               .body("[0].id", equalTo("ddl"))
               .body("[0].name", equalTo("Dulce de leche"))
               .body("[0].tipo", equalTo("dulce de leches"));
+  }
 
-
-
-//            ExtractableResponse<Response> response = RestAssured
-//      .given()
-//        .filter(new RequestLoggingFilter())
-//        .auth().preemptive().basic("duke", "secret")
-//        .contentType("application/json")
-//        .body("{\"title\": \"Effective Java\", \"isbn\":\"978-0-13-468599-1\", \"author\":\"Joshua Bloch\"}")
-//      .when()
-//        .post("http://localhost:" + port + "/api/books")
-//      .then()
-//        .statusCode(201)
-//        .extract();
-
-//    RestAssured
-//      .when()
-//        .get(response.header("Location"))
-//      .then()
-//        .statusCode(200)
-//        .body("id", Matchers.notNullValue())
-//        .body("isbn", Matchers.equalTo("978-0-13-468599-1"))
-//        .body("author", Matchers.equalTo("Joshua Bloch"))
-//        .body("title", Matchers.equalTo("Effective Java"));
+  @Test
+  void unPedidoRecienCreadoSeTieneQuePoderObtener() {
+    String direccionEntrega = "Rivadavia 1234 5B";
+    ExtractableResponse<Response> pedidoCreado = RestAssured
+            .given()
+              .contentType(ContentType.JSON)
+              .body(Map.of("direccion_entrega", direccionEntrega))
+            .when()
+              .post(apiPath() + "/pedidos")
+            .then()
+              .statusCode(201)
+              .body("direccion_entrega", equalTo(direccionEntrega))
+              .extract();
+    RestAssured
+            .when()
+              .get(pedidoCreado.header("Location"))
+            .then()
+              .statusCode(200)
+              .body("id", notNullValue())
+              .body("direccion_entrega", equalTo(direccionEntrega));
   }
 }
