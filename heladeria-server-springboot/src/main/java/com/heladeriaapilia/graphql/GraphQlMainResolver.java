2a1d71d67c70d1c8e7e174b551c354ec798f3f27
Guido de Caso <guido@dialpad.com>
2022-05-29 18:50:01 -0300

    graphql mutation

--- heladeria-server-springboot/src/main/java/com/heladeriaapilia/graphql/GraphQlMainResolver.java
+++ heladeria-server-springboot/src/main/java/com/heladeriaapilia/graphql/GraphQlMainResolver.java
@@ -1,8 +1,11 @@
 package com.heladeriaapilia.graphql;
 
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import com.heladeriaapilia.model.GustoData;
 import com.heladeriaapilia.model.PedidoData;
+import com.heladeriaapilia.model.PoteData;
+import com.heladeriaapilia.model.PoteData.PesoDePoteData;
 import com.heladeriaapilia.service.GustoService;
 import com.heladeriaapilia.service.PedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +15,13 @@ import java.util.List;
 import java.util.Optional;
 
 @Component
-public class Query implements GraphQLQueryResolver {
+public class GraphQlMainResolver implements GraphQLQueryResolver, GraphQLMutationResolver {
 
     private final GustoService gustoService;
     private final PedidoService pedidoService;
 
     @Autowired
-    public Query(GustoService gustoService, PedidoService pedidoService) {
+    public GraphQlMainResolver(GustoService gustoService, PedidoService pedidoService) {
         this.gustoService = gustoService;
         this.pedidoService = pedidoService;
     }
@@ -29,5 +32,11 @@ public class Query implements GraphQLQueryResolver {
 
     public Optional<PedidoData> findPedido(int id) {
         return pedidoService.findPedidoById(id);
+    }
+
+    public PoteData agregarPote(int pedidoId, PesoDePoteData peso, List<String> gustoIds) {
+        PedidoData pedido = findPedido(pedidoId)
+                .orElseThrow(() -> new IllegalArgumentException("No se encuentra el pedido " + pedidoId));
+        return pedidoService.addPoteToPedido(pedido, peso, gustoIds);
     }
 }
